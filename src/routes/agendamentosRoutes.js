b84722d47c4845e27f6867bd54473ae512a8e024
rafaelfreitasdeassis <rafaeldoze@hotmail.com>
2026-06-22 12:12:47 -0300

    criando agendamentosRoutes.js

--- src/routes/agendamentosRoutes.js
+++ src/routes/agendamentosRoutes.js
@@ -1,1 +1,21 @@
 
+import { Router } from 'express';
+import * as controller from '../controllers/agendamentosController.js';
+import { autenticarJWT } from '../middlewares/autenticacao.js';
+
+const router = Router();
+
+router.use(autenticarJWT);                             // todas as rotas exigem autenticação
+
+router.get(  '/',  controller.listar );                 // GET /agendamentos
+
+router.get(  '/:id',  controller.buscarPorId );        // GET /agendamentos/:id
+
+router.post(  '/',  controller.criar );                // POST /agendamentos
+
+router.put(  '/:id',  controller.atualizar );          // PUT /agendamentos/:id
+
+router.delete(  '/:id',  controller.remover);          // DELETE /agendamentos/:id
+
+export default router;
+
