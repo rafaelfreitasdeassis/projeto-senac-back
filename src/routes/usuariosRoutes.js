39a47877f2375f6debf4186119a2e965a8c77000
rafaelfreitasdeassis <rafaeldoze@hotmail.com>
2026-06-22 15:56:25 -0300

    alterando router.get listar

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -26,7 +26,7 @@ router.post('/login', controller.login);       // login → devolve JWT
 
 // rotas protegidas (precisam do header Authorization: Bearer <token>)
 router.get('/perfil', autenticarJWT, controller.perfil);   // antes de /:id
-router.get('/', controller.listar);
+router.get('/', autenticarJWT, controller.listar);
 router.get('/:id', autenticarJWT, controller.buscarPorId);
 router.put('/:id', autenticarJWT, controller.atualizar);
 router.delete('/:id', autenticarJWT, controller.remover);
