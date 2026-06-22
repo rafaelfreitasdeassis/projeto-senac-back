d90a96101a51ec6dd080d1ebe803793fedfa83aa
rafaelfreitasdeassis <rafaeldoze@hotmail.com>
2026-06-22 11:52:02 -0300

    criação do petsRoutes.js

--- src/routes/petsRoutes.js
+++ src/routes/petsRoutes.js
@@ -1,1 +1,27 @@
 
+
+import { Router } from 'express';
+import * as controller from '../controllers/petsController.js';
+import { autenticarJWT } from '../middlewares/autenticacao.js';
+
+const router = Router();
+
+router.use(autenticarJWT);    // Todas as rotas de pets exigem autenticação
+
+
+router.get( '/', controller.listar );                                 // GET /pets
+
+router.get( '/usuario/:usuarioId',  controller.listarPorUsuario );    // GET /pets/usuario/:usuarioId
+
+router.get( '/:id',  controller.buscarPorId );                        // GET /pets/:id
+
+router.post( '/',  controller.criar );                               // POST /pets
+
+router.put( '/:id',  controller.atualizar );                         // PUT /pets/:id
+
+router.delete( '/:id',  controller.remover );                        // DELETE /pets/:id
+
+
+export default router;
+
+
